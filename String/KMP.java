94320ff28caa9037112c8a57308947f5443e8605
sumon <hasanm@goldmail.etsu.edu>
2016-03-25 17:35:38 -0400

    Minor alignment adjustment

--- String/KMP.java
+++ String/KMP.java
@@ -11,7 +11,6 @@ public class KMP {
 	ArrayList<Integer> matchIndix = new ArrayList<>();
 
 	public static final String ANSI_BLUE = "\u001B[34m";
-
 	public static final String ANSI_WHITE = "\u001B[37m";
 
 	public void KMP_Matcher(String text, String pattern) {
@@ -20,57 +19,41 @@ public class KMP {
 		int pi[], lengthOfText = 0, lengthOfPattern = 0, q = 0, i;
 
 		lengthOfText = text.length(); // length of text
-
 		lengthOfPattern = pattern.length(); // length of pattern
-
 		pi = computPrefixFromPattern(pattern); // Compute the prefix function of
 												// pattern
 
 		char T[] = new char[lengthOfText + 1];
-
 		char TT[] = text.toCharArray();
 
 		for (i = 0; i < lengthOfText; i++)
-
 			T[i + 1] = TT[i];
 
 		char P[] = new char[lengthOfPattern + 1];
-
 		char PP[] = pattern.toCharArray();
 
 		for (i = 0; i < lengthOfPattern; i++)
-
 			P[i + 1] = PP[i];
-
+		
 		q = 0;
 
 		for (i = 1; i <= lengthOfText; i++) {
-
+			
 			while (q > 0 && P[q + 1] != T[i]) {
-
 				q = pi[q];
-
 			}
 
 			if (P[q + 1] == T[i])
-
 				q = q + 1;
 
 			if (q == lengthOfPattern) {
-
 				matchIndix.add(i - lengthOfPattern);
-
 				out.println("Pattern occurs with shift: "
 						+ (i - lengthOfPattern));
-
 				q = pi[q];
-
 			}
-
 		}
-
 		printPattern(text, pattern, matchIndix);
-
 	}
 
 	// display all occurance of the pattern
@@ -88,13 +71,11 @@ public class KMP {
 			out.print(colors[i] + "  ");
 
 		for (int i = 0; i < text.length(); i++) {
-
 			if (colors[i] == 1)
 				System.out.print(ANSI_BLUE + text.charAt(i) + ANSI_WHITE);
 			else
 
 				System.out.print(text.charAt(i));
-
 		}
 
 	}
@@ -103,45 +84,30 @@ public class KMP {
 	public int[] computPrefixFromPattern(String pattern) {
 
 		int len; // length of prefix
-
 		int pi[]; // prefix index of pattern
-
 		int q, k;
-
 		len = pattern.length();
 
 		char pat[] = new char[len + 1];
-
 		char p[] = pattern.toCharArray();
 
 		pi = new int[len + 1];
 
 		for (int i = 0; i < len; i++)
-
 			pat[i + 1] = p[i];
 
 		pi[1] = 0;
-
 		k = 0;
 
 		for (q = 2; q < len; q++) {
-
 			while (k > 0 && (pat[k + 1] != pat[q])) {
-
 				k = pi[k];
-
 			}
 
 			if (pat[k + 1] == pat[q])
-
 				k = k + 1;
-
 			pi[q] = k;
-
 		}
-
 		return pi;
-
 	}
-
 }
