a97cbbedc140b20d11565b7ff9f14610e5f7742d
sumon <hasanm@goldmail.etsu.edu>
2016-03-25 17:43:36 -0400

    End of File for input check

---
+++ JAVA/EOF.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int counter = 0;
+		String str;
+		
+		while( sc.hasNext()){
+		   counter++;
+		   str = sc.nextLine();
+		   out.format("%d %s", counter, str);
+		}
+
+	}
+
+}
