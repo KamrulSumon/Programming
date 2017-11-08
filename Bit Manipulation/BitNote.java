7f806a54640bd82a10b90ce5159a4b4f2d420e79
Sumon <sumon@Sumons-MacBook-Pro.local>
2017-11-07 23:54:51 -0500

    Bit Note added

---
+++ Bit Manipulation/BitNote.java
@@ -0,0 +1,20 @@
+public class BitNote {
+
+  //AND operation
+  0 & 0 => 0
+  0 & 1 => 0
+  1 & 1 => 1
+
+
+  // OR operation
+  0 | 0 => 0
+  0 | 1 => 1
+  1 | 1 => 1
+
+
+  //XOR operation
+  0 ^ 0 => 0
+  0 ^ 1 => 1
+  1 ^ 1 => 0
+
+}
